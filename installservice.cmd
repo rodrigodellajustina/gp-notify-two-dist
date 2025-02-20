b0f9892d615100c2bc8da7e3b00a14341d1bca34
della <rodrigoj@sssistemas.com>
2025-02-20 13:21:37 -0300

    change file event

--- installservice.cmd
+++ installservice.cmd
@@ -1,21 +1,21 @@
 sc.exe delete gp-notify-two
-nssm install ssedocservice %cd%\main.exe 
-nssm set ssedocservice AppDirectory %cd%\
-nssm set ssedocservice AppParameters SERVICE 
-nssm set ssedocservice DisplayName gp-notify-two
-nssm set ssedocservice Description Notificador Gestao Parts
-nssm set ssedocservice Start SERVICE_AUTO_START
-nssm set ssedocservice ObjectName LocalSystem
-nssm set ssedocservice Type SERVICE_WIN32_OWN_PROCESS
-nssm set ssedocservice AppPriority NORMAL_PRIORITY_CLASS
-nssm set ssedocservice AppNoConsole 0
-nssm set ssedocservice AppAffinity All
-nssm set ssedocservice AppStopMethodSkip 0
-nssm set ssedocservice AppStopMethodConsole 1500
-nssm set ssedocservice AppStopMethodWindow 1500
-nssm set ssedocservice AppStopMethodThreads 1500
-nssm set ssedocservice AppThrottle 1500
-nssm set ssedocservice AppExit Default Restart
-nssm set ssedocservice AppRestartDelay 0
-nssm set ssedocservice AppStdout %cd%\log\logservice.log
-nssm set ssedocservice AppStderr %cd%\log\logservice.log
+nssm install gp-notify-two %cd%\main.exe 
+nssm set gp-notify-two AppDirectory %cd%\
+nssm set gp-notify-two AppParameters SERVICE 
+nssm set gp-notify-two DisplayName gp-notify-two
+nssm set gp-notify-two Description Notificador Gestao Parts
+nssm set gp-notify-two Start SERVICE_AUTO_START
+nssm set gp-notify-two ObjectName LocalSystem
+nssm set gp-notify-two Type SERVICE_WIN32_OWN_PROCESS
+nssm set gp-notify-two AppPriority NORMAL_PRIORITY_CLASS
+nssm set gp-notify-two AppNoConsole 0
+nssm set gp-notify-two AppAffinity All
+nssm set gp-notify-two AppStopMethodSkip 0
+nssm set gp-notify-two AppStopMethodConsole 1500
+nssm set gp-notify-two AppStopMethodWindow 1500
+nssm set gp-notify-two AppStopMethodThreads 1500
+nssm set gp-notify-two AppThrottle 1500
+nssm set gp-notify-two AppExit Default Restart
+nssm set gp-notify-two AppRestartDelay 0
+nssm set gp-notify-two AppStdout %cd%\log\logservice.log
+nssm set gp-notify-two AppStderr %cd%\log\logservice.log
